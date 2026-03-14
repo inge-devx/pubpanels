663ee2d1e683014f7dca2f3e872c1f736ab226bc
Inge Devx <ingenielix@gmail.com>
2026-03-14 12:58:48 +0000

    Set up base Django structure with custom user and core app

---
+++ apps/users/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        SUPER_ADMIN = "super_admin", "Super Admin"
+        AGENCY_ADMIN = "agency_admin", "Agency Admin"
+        AGENCY_MANAGER = "agency_manager", "Agency Manager"
+
+    role = models.CharField(
+        max_length=30,
+        choices=Role.choices,
+        default=Role.AGENCY_MANAGER,
+    )
+
+    def __str__(self):
+        return self.username
